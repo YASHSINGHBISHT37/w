dad767385ce3527de6f44170069bab09e39f95b6
yash singh bisht <yashbisht0007@gmail.com>
2025-09-23 04:58:02 +0530

    .

--- src/assets/SetCity.jsx
+++ src/assets/SetCity.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const SetCity = () => {
     const [focus, setFocus] = useState({ search: false, lat: false, lon: false, city: false, district: false })
