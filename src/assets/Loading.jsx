f03e018396254618640d1ab349c8abbfdc495a6c
yash singh bisht <yashbisht0007@gmail.com>
2025-09-23 05:03:04 +0530

    Loading

--- src/assets/Loading.jsx
+++ src/assets/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='flex items-center flex-col pt-[80vw] w-screen h-screen absolute top-0 bg-gradient-to-b from-blue-300 to-blue-900 hover:z-[-1]'>
+        <div className='flex items-center flex-col pt-[80vw] w-screen h-screen absolute top-0 bg-gradient-to-b from-blue-300 to-blue-900'>
             <img src="weather-icons/clear-day.svg" alt="sun-loading" className='w-36 h-36' />
             <p class="tracking-tight leading-[2vh] mt-[-2vw] text-center text-white/90">Don’t let the weather surprise you — stay <br /> prepared every day!</p>
         </div>
